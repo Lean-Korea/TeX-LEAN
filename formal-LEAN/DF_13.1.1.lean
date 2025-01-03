d7e9a8572bd9824a3bbec2ad1f561f70e6bbb810
ychpark-ks <150765600+ychpark-ks@users.noreply.github.com>
2025-01-03 09:43:14 +0900

    Update DF_13.1.1.lean

--- formal-LEAN/DF_13.1.1.lean
+++ formal-LEAN/DF_13.1.1.lean
@@ -26,6 +26,7 @@ theorem DF_13_1_3 : Irreducible p3 :=
 sorry
 
 -- DF_13_1_5 
+-- Suppose $\alpha$ is a rational root of a monic polynomial in $\mathbb{Z}[x]$. Prove that $\alpha$ is an integer.
 variable {α : ℚ} {f : Polynomial ℤ}
 variable (monic_f : f.Monic) (α_root : (f.map (Int.castRingHom ℚ)).eval α = 0)
 theorem DF_13_1_35 : ∃ (n : ℤ), α = n := sorry
