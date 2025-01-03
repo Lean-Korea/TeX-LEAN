0007b6a02613c6214f294bd3f0ab25befb053694
ychpark-ks <150765600+ychpark-ks@users.noreply.github.com>
2025-01-03 09:24:32 +0900

    Update DF_13.1.1.lean

--- formal-LEAN/DF_13.1.1.lean
+++ formal-LEAN/DF_13.1.1.lean
@@ -1,9 +1,16 @@
 import Mathlib
 import Mathlib.Algebra.Polynomial.Basic
 
--- Define the polynomial p(x) = x^3 + 9x + 6 over ℚ
-noncomputable def p : Polynomial ℚ := Polynomial.X ^ 3 + 9 * Polynomial.X + 6
+-- Define the polynomial p1(x) = x^3 + 9x + 6 over ℚ
+noncomputable def p1 : Polynomial ℚ := Polynomial.X ^ 3 + 9 * Polynomial.X + 6
 
--- Theorem: p(x) is irreducible in ℚ[x]
-theorem p_irreducible : Irreducible p := 
+-- Theorem: p1(x) is irreducible in ℚ[x]
+theorem DF_13_1_1 : Irreducible p1 := 
+sorry
+
+-- Define the polynomial p2(x) = x^3 -2x -2 over ℚ
+noncomputable def p2 : Polynomial ℚ := Polynomial.X ^ 3 - 2 * Polynomial.X - 2
+
+-- Theorem: p2(x) is irreducible in ℚ[x]
+theorem DF_13_1_2 : Irreducible p2 := 
 sorry
