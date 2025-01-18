ce852fcede3f67070ca62f4c64c3e80c391fced3
Seewoo Lee <seewoo5@gmail.com>
2025-01-18 11:15:53 -0800

    open Polynomial

--- Formal/DF_13/DF_13_1_2.lean
+++ Formal/DF_13/DF_13_1_2.lean
@@ -2,9 +2,10 @@ import Mathlib.Algebra.Order.Ring.Rat
 import Mathlib.Algebra.Polynomial.Basic
 import Mathlib.Algebra.Prime.Defs
 
+
+open Polynomial
+
 -- DF_13_1_2
 -- Show that $p(x) = x^3 -2x-2$ is irreducible in $ℚ[x]$.
 
-noncomputable def p2 : Polynomial ℚ := Polynomial.X ^ 3 - 2 * Polynomial.X - 2
-
-theorem DF_13_1_2 : Irreducible p2 := sorry
+theorem DF_13_1_2 : Irreducible (X ^ 3 - 2 * X - 2 : Polynomial ℚ) := sorry
