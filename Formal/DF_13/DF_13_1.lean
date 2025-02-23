ac1613b05a0ebf35f4ac0a44886a5125d7bccc79
Jineon Baek <jineon@umich.edu>
2025-02-23 13:11:54 +0900

    credit the right person

--- Formal/DF_13/DF_13_1.lean
+++ Formal/DF_13/DF_13_1.lean
@@ -6,17 +6,33 @@ import Mathlib.Algebra.Polynomial.Eval.Defs
 
 open scoped Polynomial
 
-/-- Show that $p(x) = x^3 + 9x + 6$ is irreducible in $ℚ[x]$. -/
+/--
+Show that $p(x) = x^3 + 9x + 6$ is irreducible in $ℚ[x]$.
+
+Contributor: Yeachan Park
+-/
 theorem DF_13_1_1 : Irreducible (X^3 + 9*X + 6 : Polynomial ℚ) := sorry
 
-/-- Show that $p(x) = x^3 - 2x - 2$ is irreducible in $ℚ[x]$. -/
+/--
+Show that $p(x) = x^3 - 2x - 2$ is irreducible in $ℚ[x]$.
+
+Contributor: Yeachan Park
+-/
 theorem DF_13_1_2 : Irreducible (X^3 - 2*X - 2 : Polynomial ℚ) := sorry
 
-/-- Show that $p(x) = x^3 + x + 1$ is irreducible in $𝔽_2[x]$. -/
+/--
+Show that $p(x) = x^3 + x + 1$ is irreducible in $𝔽_2[x]$.
+
+Contributor: Yeachan Park
+-/
 theorem DF_13_1_3 : Irreducible (X^3 + X + 1 : Polynomial (ZMod 2)) := sorry
 
-/-- Suppose $\alpha$ is a rational root of a monic polynomial in $\mathbb{Z}[x]$.
-Prove that $\alpha$ is an integer. -/
+/--
+Suppose $\alpha$ is a rational root of a monic polynomial in $\mathbb{Z}[x]$.
+Prove that $\alpha$ is an integer.
+
+Contributor: Yeachan Park
+-/
 theorem DF_13_1_5 {α : ℚ} {f : Polynomial ℤ}
   (monic_f : f.Monic) (α_root : (f.map (Int.castRingHom ℚ)).eval α = 0)
  : ∃ (n : ℤ), α = n := sorry
