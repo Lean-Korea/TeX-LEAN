db49718390b6a5941ece63183781019f9f1df1ef
Jineon Baek <jineon@umich.edu>
2025-02-23 14:20:50 +0900

    update Formal/DF.lean

--- Formal/DF.lean
+++ Formal/DF.lean
@@ -1,10 +1,7 @@
 import Formal.DF_1.DF_1_4_1
 import Formal.DF_1.DF_1_4_7
 import Formal.DF_7.DF_7_1_1
-import Formal.DF_13.DF_13_1_1
-import Formal.DF_13.DF_13_1_2
-import Formal.DF_13.DF_13_1_3
-import Formal.DF_13.DF_13_1_5
+import Formal.DF_13.DF_13_1
 import Formal.DF_18.DF_18_1_1
 import Formal.DF_18.DF_18_1_5
 import Formal.DF_18.DF_18_3_1
