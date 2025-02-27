d90c24aa252a4e5cbf6b124531483dc3d0b2edb6
Ilkyoo <ilkyoochoi@gmail.com>
2025-02-28 00:37:49 +0900

    Update DF.lean
    cleanup DF.lean

--- Formal/DF.lean
+++ Formal/DF.lean
@@ -3,5 +3,4 @@ import Formal.DF_1.DF_1_4_7
 import Formal.DF_7.DF_7_1_1
 import Formal.DF_13.DF_13_1
 import Formal.DF_18.DF_18_1
-import Formal.DF_18.DF_18_3_1
-import Formal.DF_18.DF_18_3_6
+import Formal.DF_18.DF_18_3
