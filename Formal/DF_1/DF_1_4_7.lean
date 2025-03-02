c89008e794514dcee5bcb01ac52b76b18eb2da36
Seewoo Lee <seewoo5@gmail.com>
2025-03-02 14:01:37 -0800

    reformat 1.4

--- Formal/DF_1/DF_1_4_7.lean
+++
@@ -1,9 +0,0 @@
-import Mathlib.Algebra.Field.Defs
-import Mathlib.LinearAlgebra.Matrix.GeneralLinearGroup.Defs
-
-
-variable {p : ℕ} [Fact (Nat.Prime p)]
-
-instance : Field (ZMod p) := ZMod.instField p
-
-theorem DF_1_4_7 : Nat.card (Matrix.GeneralLinearGroup (Fin 2) (ZMod p)) = p^4 - p^3 - p^2 + p := sorry
