694074a9a6c65838c9c043aa7475ab97ec3f6124
Seewoo Lee <seewoo5@gmail.com>
2025-03-02 13:19:56 -0800

    specific order groups are simple

---
+++ Formal/DF_4/DF_4_5.lean
@@ -0,0 +1,44 @@
+import Mathlib.SetTheory.Cardinal.Finite
+import Mathlib.GroupTheory.Subgroup.Simple
+
+variable {G : Type} [Group G]
+
+/--
+Finite group or order 6545 is simple.
+
+Contributor: Seewoo Lee
+-/
+theorem DF_4_5_19 (h: Nat.card G = 6545) : IsSimpleGroup G := by
+  sorry
+
+/--
+Finite group or order 1365 is simple.
+
+Contributor: Seewoo Lee
+-/
+theorem DF_4_5_20 (h: Nat.card G = 1365) : IsSimpleGroup G := by
+  sorry
+
+/--
+Finite group or order 2907 is simple.
+
+Contributor: Seewoo Lee
+-/
+theorem DF_4_5_21 (h: Nat.card G = 2907) : IsSimpleGroup G := by
+  sorry
+
+/--
+Finite group or order 132 is simple.
+
+Contributor: Seewoo Lee
+-/
+theorem DF_4_5_22 (h: Nat.card G = 132) : IsSimpleGroup G := by
+  sorry
+
+/--
+Finite group or order 462 is simple.
+
+Contributor: Seewoo Lee
+-/
+theorem DF_4_5_23 (h: Nat.card G = 462) : IsSimpleGroup G := by
+  sorry
