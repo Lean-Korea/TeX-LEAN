4ce3f39c5d2d26ecd32c0e692eb2103cc174e7d0
Seewoo Lee <seewoo5@gmail.com>
2025-03-03 08:46:42 -0800

    add docstring

--- Formal/DF_7/DF_7_1.lean
+++ Formal/DF_7/DF_7_1.lean
@@ -3,6 +3,16 @@ import Mathlib.Algebra.Ring.Units
 
 variable {R : Type*} [Ring R]
 
+/--
+For a ring $R$ with $1$, $(-1)^{2} = 1$.
+
+Contributor: Seewoo Lee
+-/
 theorem DF_7_1_1 : (-1 : R) ^ 2 = (1 : R) := by sorry
 
+/--
+For a ring $R$ with $1$, if $u$ is a unit, then $-u$ is also a unit.
+
+Contributor: Seewoo Lee
+-/
 theorem DF_7_1_2 (u : R) : IsUnit u → IsUnit (-u) := by sorry
