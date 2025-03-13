9a6d544c3ae09bb7637670ed665f131b15dad68a
Byung-Hak <byunghakhwang@gmail.com>
2025-03-13 15:32:10 +0900

    Add DF_15_2_7

--- Formal/DF.lean
+++ Formal/DF.lean
@@ -2,5 +2,6 @@ import Formal.DF_1.DF_1_4
 import Formal.DF_2.DF_2_1
 import Formal.DF_7.DF_7_1
 import Formal.DF_13.DF_13_1
+import Formal.DF_15.DF_15_2
 import Formal.DF_18.DF_18_1
 import Formal.DF_18.DF_18_3
---
+++ Formal/DF_15/DF_15_2.lean
@@ -0,0 +1,25 @@
+import Mathlib.Order.CompletePartialOrder
+import Mathlib.RingTheory.Ideal.Maps
+
+variable {R : Type*} [CommRing R]
+variable {S : Type*} [CommRing S]
+
+
+/--
+Let $R$ and $S$ be commutative rings, and $f: R\rightarrow S$ a ring homomorphism.
+Let $I$ be an ideal of $R$. Then $f(rad(I)) \subseteq rad(f(I))$, where $rad(J)$ is the radical of an ideal $J$.
+
+Contributor: Byung-Hak Hwang
+-/
+theorem DF_15_2_7_1 (f : R →+* S) (I : Ideal R) : Ideal.map f (I.radical) ≤ (Ideal.map f I).radical := by
+  sorry
+
+/--
+Let $R$ and $S$ be commutative rings, and $f: R\rightarrow S$ a surjective ring homomorphism.
+Let $I$ be an ideal of $R$ such that $I$ contains the kernel of $f$.
+Then $f(rad(I)) = rad(f(I))$, where $rad(J)$ is the radical of an ideal $J$.
+
+Contributor: Byung-Hak Hwang
+-/
+theorem DF_15_2_7_2 (f : R →+* S) (hf : Function.Surjective f) (I : Ideal R) (hI : RingHom.ker f ≤ I) : Ideal.map f (I.radical) = (Ideal.map f I).radical := by
+  sorry
