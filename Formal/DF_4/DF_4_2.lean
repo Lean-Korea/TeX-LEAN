bcbb40189136a63a0a307303c9dbd20781f36e75
Seewoo Lee <seewoo5@gmail.com>
2025-03-20 18:15:20 -0700

    remove variable

--- Formal/DF_4/DF_4_2.lean
+++ Formal/DF_4/DF_4_2.lean
@@ -1,6 +1,5 @@
 import Mathlib.GroupTheory.Index
 
-variable {G : Type*} [Group G]
 
 /--
 If $H$ is a finite index subgroup of $G$ of index $n$, then there exists
@@ -8,6 +7,6 @@ a normal subgroup $K$ of $G$ such that $K \leq H$ and $[G : K] \leq n!$.
 
 Contributor: Kyu-Hwan Lee, Seewoo Lee
 -/
-theorem DF_4_2_8 {H : Subgroup G} {n : Nat} (hH : H.index = n)
+theorem DF_4_2_8 [Group G] {H : Subgroup G} {n : Nat} (hH : H.index = n)
     : ∃ (K : Subgroup G), K.Normal ∧ K ≤ H ∧ K.index ≤ Nat.factorial n := by
   sorry
