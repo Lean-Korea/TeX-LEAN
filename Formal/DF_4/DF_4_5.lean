5be41196aa97dfd5d95f1c4d1d263e4a38103b78
Ilkyoo <ilkyoochoi@gmail.com>
2025-03-21 10:38:35 +0900

    Update DF_4_5.lean
    or->of

--- Formal/DF_4/DF_4_5.lean
+++ Formal/DF_4/DF_4_5.lean
@@ -2,7 +2,7 @@ import Mathlib.GroupTheory.SpecificGroups.Alternating
 
 
 /--
-Finite group or order 6545 is not simple.
+Finite group of order 6545 is not simple.
 
 Contributor: Seewoo Lee
 -/
@@ -10,7 +10,7 @@ theorem DF_4_5_19 [Group G] (h: Nat.card G = 6545) : ¬ IsSimpleGroup G := by
   sorry
 
 /--
-Finite group or order 1365 is not simple.
+Finite group of order 1365 is not simple.
 
 Contributor: Seewoo Lee
 -/
@@ -18,7 +18,7 @@ theorem DF_4_5_20 [Group G] (h: Nat.card G = 1365) : ¬ IsSimpleGroup G := by
   sorry
 
 /--
-Finite group or order 2907 is not simple.
+Finite group of order 2907 is not simple.
 
 Contributor: Seewoo Lee
 -/
@@ -26,7 +26,7 @@ theorem DF_4_5_21 [Group G] (h: Nat.card G = 2907) : ¬ IsSimpleGroup G := by
   sorry
 
 /--
-Finite group or order 132 is not simple.
+Finite group of order 132 is not simple.
 
 Contributor: Seewoo Lee
 -/
@@ -34,7 +34,7 @@ theorem DF_4_5_22 [Group G] (h: Nat.card G = 132) : ¬ IsSimpleGroup G := by
   sorry
 
 /--
-Finite group or order 462 is not simple.
+Finite group of order 462 is not simple.
 
 Contributor: Seewoo Lee
 -/
