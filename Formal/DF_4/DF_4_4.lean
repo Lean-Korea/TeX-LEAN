4bd6f2168692da46c436dfb48a1c76a255e4e6b0
ByungHakHwang <34232221+ByungHakHwang@users.noreply.github.com>
2025-03-21 19:25:17 +0900

    Update DF_4_4.lean
    Replace { } by ( )

--- Formal/DF_4/DF_4_4.lean
+++ Formal/DF_4/DF_4_4.lean
@@ -2,10 +2,10 @@ import Mathlib.Data.Nat.Prime.Defs
 import Mathlib.SetTheory.Cardinal.Finite
 
 /--
-Let $p$ and $q$ be distinct primes. Let $G$ be a group of order $pq$.
+Let $p$ and $q$ be distinct primes. Let $G$ be an abelian group of order $pq$.
 Then $G$ is cyclic.
 
 Contributor: Byung-Hak Hwang
 -/
-theorem DF_4_4_2 [CommGroup G] {p q : Nat} {hp : Nat.Prime p} {hq : Nat.Prime q} {hdistinct : p ≠ q} {hG : Nat.card G = p * q} : IsCyclic G := by
+theorem DF_4_4_2 [CommGroup G] {p q : Nat} (hp : Nat.Prime p) (hq : Nat.Prime q) (hdistinct : p ≠ q) (hG : Nat.card G = p * q) : IsCyclic G := by
   sorry
