d3c8c71eebfdb94249d5fffc41bb6d602c2da9e5
Kyu-Hwan Lee <khlee@P36KN.local>
2025-03-23 18:15:16 -0400

    DF_10_1 #1-4

--- Formal/DF.lean
+++ Formal/DF.lean
@@ -1,6 +1,7 @@
 import Formal.DF_1.DF_1_4_1
 import Formal.DF_1.DF_1_4_7
 import Formal.DF_7.DF_7_1_1
+import Formal.DF_10.DF_10_1
 import Formal.DF_13.DF_13_1
 import Formal.DF_18.DF_18_1
 import Formal.DF_18.DF_18_3
