ec69be6ff6e236c23998b4262161706ab5e1d24e
ychpark-ks <150765600+ychpark-ks@users.noreply.github.com>
2025-03-24 22:07:00 +0900

    Update DF_13_2.lean

--- Formal/DF_13/DF_13_2.lean
+++ Formal/DF_13/DF_13_2.lean
@@ -11,8 +11,8 @@ $p(x) = x^3-2$ and $q(x)=x^3-3$ are irreducible in $\mathbb{Q}[i]$.
 
 Contributor: Yeachan Park
 -/
-noncomputable def poly : ℚ[X] := X^2 + 1
-abbrev Q_i := AdjoinRoot poly
+noncomputable def DF_13_2_5_poly : ℚ[X] := X^2 + 1
+abbrev Q_i := AdjoinRoot DF_13_2_5_poly
 
 theorem DF_13_2_5 : Irreducible (X^3 - C 2 : Q_i[X]) ∧  Irreducible (X^3 - C 3 : Q_i[X]) := by
   sorry
